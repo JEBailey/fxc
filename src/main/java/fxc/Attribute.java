02b62cdd0ae38c44f3780c6aaaafd9ed1ab46881
jebailey <jason.bailey@24601.org>
2015-05-04 14:51:51 -0400

    Adding Apache License

--- src/main/java/fxc/Attribute.java
+++ src/main/java/fxc/Attribute.java
@@ -1,8 +1,23 @@
 package fxc;
 
+/*
+ * Copyright 2015 Jason E Bailey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
- * Represents an attribute in an xml element. This can be either
- * a single element or a key and value pair.
+ * Represents an attribute in an xml element. This can be either a single
+ * element or a key and value pair.
  * 
  * 
  * @author Jason E Bailey
@@ -29,9 +44,9 @@ public class Attribute {
 	public Attribute(String key, Object value) {
 		this(key, String.valueOf(value));
 	}
-	
-	public boolean update(Attribute other){
-		if (!this.equals(other)){
+
+	public boolean update(Attribute other) {
+		if (!this.equals(other)) {
 			return false;
 		}
 		this.value = other.value;
@@ -45,7 +60,7 @@ public class Attribute {
 		result = prime * result + ((key == null) ? 0 : key.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
@@ -54,15 +69,15 @@ public class Attribute {
 		if (obj == null) {
 			return false;
 		}
-		if (getClass() != obj.getClass()){
+		if (getClass() != obj.getClass()) {
 			return false;
 		}
 		Attribute other = (Attribute) obj;
 		if (key == null) {
-			if (other.key != null){
+			if (other.key != null) {
 				return false;
 			}
-		} else if (!key.equals(other.key)){
+		} else if (!key.equals(other.key)) {
 			return false;
 		}
 		return true;
